0da26725ecefe54249cbe63ec41d44cb7184a5bf
nourfouad23 <n.fouad@xtoniq.com>
2023-11-02 15:56:26 -0700

    args file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_arg(argv):
+    num = len(argv) - 1
+    if num == 0:
+        print("{:d} argument.".format(num))
+        return
+    else:
+        if num == 1:
+            print("{:d} argument:".format(num))
+        else:
+            print("{:d} arguments:".format(num))
+        i = 1
+        while i <= num:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
