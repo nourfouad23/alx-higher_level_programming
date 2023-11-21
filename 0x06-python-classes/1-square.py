2f582fa88e03665102e2418318ea5e92e9918716
nourfouad23 <n.fouad@xtoniq.com>
2023-11-21 14:21:22 -0800

    implements class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class."""
+
+
+class Square:
+    """Present a class"""
+
+    def __init__(self, size):
+        """Initialize a new instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
