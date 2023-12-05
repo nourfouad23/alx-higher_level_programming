43e7ac1fe9f61d320c1f67dffa8592e5de51d151
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-12-06 01:00:26 +0200

    triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creates Pascal's Triangle func"""
+
+
+def pascal_triangle(n):
+    """Show Pascal's Triangle of size n.
+    Returns list to show triangle
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
