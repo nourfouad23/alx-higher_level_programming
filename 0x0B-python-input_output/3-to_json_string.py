8d392f84e2f1a2dcc8c0a7b774902068584cef97
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-12-06 00:53:52 +0200

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' JSON presentation of obj
+'''
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' to json string func
+     return JSON
+    '''
+    return json.dumps(my_obj)
