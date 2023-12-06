7a5a7d370cfb39b0009290443b4c5b867cd2d700
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-12-07 01:31:31 +0200

    class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''function: is_kind_of_class
+    obj: param
+    a_class: class
+    Returns: true or false
+    '''
+    return isinstance(obj, a_class)
