b04c5e8acf51ddd2bd935fb6c744e8211b740d57
Nour Fouad <Nour.Fouad@Qcentriseg.com>
2023-12-07 01:32:40 +0200

    geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a geometry class"""
+
+
+class BaseGeometry:
+    """Shows base geometry"""
+
+    def area(self):
+        """No implementation"""
+        raise Exception("area() is not implemented")
